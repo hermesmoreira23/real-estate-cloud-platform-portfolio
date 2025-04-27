29ad952c278c889fc50b456cbb531d4383267cbf
Hermesmoreira <hermesmoreira23@gmail.com>
2025-04-27 17:50:11 +0200

    Agregar variables.tf y outputs.tf para modularizar infraestructura

---
+++ terraform/networking/main.tf
@@ -0,0 +1,57 @@
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Cree una VPC basica
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "real-estate-vpc"
+    Environment = "development"
+  }
+}
+
+# Cree un Internet Gateway para dar acceso a internet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "real-estate-igw"
+  }
+}
+
+# Cree una Subnet publica
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "real-estate-public-subnet"
+  }
+}
+
+# Cree una tabla de ruteo publica
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "real-estate-public-rt"
+  }
+}
+
+# Asocie la Subnet publica a la tabla de ruteo publica
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
