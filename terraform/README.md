2c90fe2167116df769e6da5743c728e4f21d8de5
Hermesmoreira <hermesmoreira23@gmail.com>
2025-05-15 16:25:24 +0200

    feat: infraestructura base con EC2, VPC y SG configurados

--- terraform/README.md
+++
@@ -1,4 +0,0 @@
-# real-estate-cloud-platform
-Repositorio Github
-
-Author: Hermes Moreira
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,63 @@ variable "key_name" {
   description = "SSH key name to access EC2"
   type        = string
 }
+variable "db_username" {
+  description = "Nombre de usuario para la base de datos PostgreSQL"
+  type        = string
+}
+
+variable "my_ip_cidr" {
+  description = "Tu IP pública con notación CIDR para acceso SSH"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Nombre de la base de datos PostgreSQL"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Contraseña del usuario administrador de la base de datos"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "Tipo de instancia de la base de datos"
+  type        = string
+}
+
+variable "db_allocated_storage" {
+  description = "Espacio de almacenamiento en GB"
+  type        = number
+}
+
+variable "db_engine_version" {
+  description = "Versión del motor PostgreSQL"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Nombre para la VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block para la VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block para la subnet pública"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Zona de disponibilidad para la subnet"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Nombre de la subnet pública"
+  type        = string
+}
