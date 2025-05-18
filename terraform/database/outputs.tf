31def109ed3dbe0300bb5d36ef3f9f3598e5f03f
Hermesmoreira <hermesmoreira23@gmail.com>
2025-05-18 15:43:13 +0200

    chore: guardar infraestructura base y outputs validados antes de añadir S3

--- terraform/database/outputs.tf
+++ terraform/database/outputs.tf
@@ -1,3 +1,11 @@
-output "rds_endpoint" {
+output "db_endpoint" {
   value = aws_db_instance.postgres.endpoint
+}
+
+output "db_name" {
+  value = aws_db_instance.postgres.db_name
+}
+
+output "db_username" {
+  value = aws_db_instance.postgres.username
 }
