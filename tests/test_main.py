8bca5dc33c03e45ba8484a6b9bd0d0eafa3c6c3d
Hermesmoreira <hermesmoreira23@gmail.com>
2025-06-08 11:52:36 +0200

    FastAPI Dockerized Backend with PostgreSQL

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 # tests/test_main.py
 
 from fastapi.testclient import TestClient
-from app.main import app     # Si main.py está bajo app/
+from app.main import app   # Importa tu FastAPI desde el paquete app
 
 client = TestClient(app)
 
@@ -9,3 +9,4 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, Real Estate Cloud!"}
+
