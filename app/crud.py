1081364b96d1aeff718176a98a5dfe0ca2dc9ad6
Hermesmoreira <hermesmoreira23@gmail.com>
2025-06-08 13:22:24 +0200

    tests

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-import models, schemas
+from . import models, schemas
 
 def create_property(db: Session, property: schemas.PropertyCreate):
     db_property = models.Property(**property.dict())
